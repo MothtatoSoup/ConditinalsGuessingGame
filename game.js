6d21d4d3ed990915669dd86258ed5edc7cc849a7
MothtatoSoup <maddy.puryear@outlook.com>
2025-02-13 11:41:29 -0500

    Found the issue, number wasn't cap

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 let randomNumber = Math.floor(Math.random() * 10) + 1;
 let attempts = 0;
 let win = false;
-// Prompt user for a number between 1 and 10, or type 999 to exit.
-let userGuess = number(prompt("Guess a random number between 1 + 10, or 999 to exit: "));
+
+
 // Create while loop that continues for as long as the user guesses are incorrect.
 while(userGuess != randomNumber){
-
+    let userGuess = Number(prompt("Guess a random number between 1 + 10, or 999 to exit: ")); // Prompt user for a number between 1 and 10, or type 999 to exit.
     // Loop should exit if user enters 999 and not display "Too high!"
     if(userGuess == 999){
         break;
